8ee980ed97d8a8963dfb4d575dea189f9f2c6171
Famamlay1919 <Famalay1919@users.noreply.github.com>
2019-02-04 10:52:16 -0400

    Added source file

--- Lab 1 intro.cpp
+++ Lab 1 intro.cpp
@@ -2,8 +2,25 @@
 #include <vector>
 using namespace std;
 main()
-vector <char>vcomp;
-vector <int> v2 = {10,14,32,64,16};
+{
+	vector <char>vcomp;
+	vector <int> v2 = {10,14,32,64,16};
+	vcomp.push_back('p');
+	v2.pop_back();
+	vcomp.push_back('i');
+	v2.pop_back();
+
+	for(int i=0; i<4; i++)
+	{
+		v2.push_back(0);
+	}
+
+	for(autov:v2)
+	{
+		cout << v << " ";	
+	}
+}
+
 
 
 
