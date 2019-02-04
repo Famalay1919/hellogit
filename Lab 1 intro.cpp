51103074e202e4493bbbb12d25e68ebe11c0c7a1
Famamlay1919 <Famalay1919@users.noreply.github.com>
2019-02-04 11:40:06 -0400

    Added source file

--- Lab 1 intro.cpp
+++ Lab 1 intro.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 main()
-{
+/*{
 	vector <char>vcomp;
+	vector<int> v;
 	vector <int> v2 = {10,14,32,64,16};
 	vcomp.push_back('p');
 	v2.pop_back();
@@ -15,12 +17,30 @@ main()
 		v2.push_back(0);
 	}
 
-	for(autov:v2)
+	for(auto v:v2)
 	{
 		cout << v << " ";	
 	}
-}
+}*/
+{
+	string s0 = " nothing";
+	string s1 = " hello";
+	string s2 = s1 + " world";
 
+	cout << "s0 =" << s0 << endl;
+	cout << "s1 =" << s1 << endl;
+	cout << "The 4th character of s1 is " << s1[3] << endl;
+	cout << "s1 has " << s1.size() << " characters" << endl;
+	
+	if(s1=="hello")
+	{
+		cout << "hi there" << endl;
+	}
+	if (s2!= "world")
+	{
+		cout << "out of this world" << endl;
+	}
+}
 
 
 
